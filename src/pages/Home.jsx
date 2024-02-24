0f20783f19ee5e6547ccb909bc8ccf9f327daac9
alx-yng <alexyoung0119@gmail.com>
2024-02-24 14:22:05 -0600

    added footer and background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header.jsx";
 import ImgCard from "../components/ImgCard.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
           <ImgCard />
         </div>
       </div>
+      <Footer />
     </>
   );
 }
