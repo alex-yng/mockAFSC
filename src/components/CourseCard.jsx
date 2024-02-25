b9bd4fde6e7e1a689535a614403270d2bdeae2f2
alx-yng <alexyoung0119@gmail.com>
2024-02-24 20:03:02 -0600

    added class titles and descriptions

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,19 @@
+export default function CourseCard({
+  img = "https://fakeimg.pl/700x500",
+  title = "Course Title",
+  description = "This is a default course description",
+}) {
+  return (
+    <>
+      <div className="size-[20rem] md:size-[22rem] lg:size-[26rem] xl:size-[32rem] bg-slate-200 rounded-lg shadow-lg hover:scale-110 duration-700">
+        <img className="p-2 aspect-[7/4]" src={img} />
+        <div className="text-slate-700 mx-2">
+          <h2 className="text-xl lg:text-3xl font-medium lg:tracking-wide mb-2">
+            {title}
+          </h2>
+          <p className="text-base lg:text-xl">{description}</p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header.jsx";
-import ImgCard from "../components/ImgCard.jsx";
+import CourseCard from "../components/CourseCard.jsx";
 import Footer from "../components/Footer.jsx";
 import HalfCard from "../components/HalfCard.jsx";
 import Icons from "../components/Icons.jsx";
@@ -30,17 +30,35 @@ export default function Home() {
       </section>
 
       {/* Course Catalog */}
-      <section className="container mx-auto mt-8 mb-32 lg:max-w-[78rem] md:max-w-[45rem]">
+      <section className="container mx-auto mt-8 mb-32 xl:max-w-[74rem] lg:max-w-[60rem] md:max-w-[45rem]">
         <h1 className="text-slate-200 text-center text-4xl font-semibold tracking-wide">
           Class Catalog
         </h1>
-        <div className="my-8 grid row-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
-          <ImgCard description="hello world" />
-          <ImgCard />
-          <ImgCard />
-          <ImgCard />
-          <ImgCard />
-          <ImgCard />
+        <div className="my-8 grid row-auto grid-cols-1 md:grid-cols-2 gap-8 place-items-center">
+          <CourseCard
+            title="Snowplow Sam (L1-4)"
+            description="Designed for children ages 3-6 with no prior skating experience to build confidence while learning the basic skills of skating."
+          />
+          <CourseCard
+            title="Basic Classes (L1-6)"
+            description="These levels introduce the fundamental moves of ice skating: forward and backward skating, stops, edges, crossovers, three turns, Mohawks and spins.  Basic 1 is for beginners and those with little experience and above the age of 6."
+          />
+          <CourseCard
+            title="Freestyle Classes - Pre-Free Skate – Freeskate 6"
+            description="Introduction to more advanced figure skating skills. Skater must complete Pre-Freeskate to register."
+          />
+          <CourseCard
+            title="​Adult Classes (L1-6)"
+            description="Teen through senior citizens can enjoy learning beginner through advanced skills."
+          />
+          <CourseCard
+            title="Hockey (L1-4)"
+            description="Fundamentals of hockey to help maneuver faster and be more agile on the ice. All elements will be taught without sticks."
+          />
+          <CourseCard
+            title="Adult Classes (L1-6)"
+            description="Teen through senior citizens can enjoy learning beginner through advanced skills."
+          />
         </div>
       </section>
 
