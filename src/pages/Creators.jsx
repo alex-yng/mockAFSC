7d6e30dc4c51c71f680c2dd34bc46afe4c8152b5
alx-yng <alexyoung0119@gmail.com>
2024-03-01 16:41:04 -0600

    added color contrast to creators page

--- src/pages/Creators.jsx
+++ src/pages/Creators.jsx
@@ -38,16 +38,16 @@ export default function Creators() {
       </div>
 
       {/* Content */}
-      <main className="flex flex-col bg-slate-700 lg:grid lg:grid-cols-2">
+      <main className="flex flex-col bg-slate-900 lg:grid lg:grid-cols-2">
         {/* Person 1 */}
-        <div className="bg-slate-700 animate-slideRight grid place-items-center">
-          <div className="flex lg:flex-col mt-8 md:mt-16 lg:mt-0 justify-center items-center lg:items-start px-8 lg:p-16 gap-8 xl:gap-16 sm:max-w-[80%] lg:max-w-full">
+        <div className="bg-slate-800 animate-slideRight grid place-items-center">
+          <div className="flex lg:flex-col lg:mt-0 justify-center items-center lg:items-start p-8 lg:p-16 gap-8 xl:gap-16 sm:max-w-[80%] lg:max-w-full">
             <h1 className="text-slate-200 text-2xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-medium tracking-wide">
               Alex
               <br />
               Young
             </h1>
-            <p className="text-white text-sm sm:text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+            <p className="text-slate-300 text-sm sm:text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
               I am an ambitious software developer and student with experience
               in languages like Java, Python, C#, HTML/CSS/Javascript,
               technologies like React, TailwindCSS, as well as leadership roles
@@ -63,14 +63,14 @@ export default function Creators() {
         </div>
 
         {/* Person 2 */}
-        <div className="lg:hidden bg-slate-700 animate-[slideRight_1.5s]">
-          <div className="flex lg:flex-col justify-center items-center lg:items-start p-8 lg:p-16 gap-8 xl:gap-16">
+        <div className="lg:hidden bg-slate-800 animate-[slideRight_1.5s] grid place-items-center">
+          <div className="flex lg:flex-col justify-center items-center lg:items-start p-8 lg:p-16 gap-8 xl:gap-16 sm:max-w-[80%] lg:max-w-full">
             <h1 className="text-slate-200 text-2xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-medium tracking-wide">
               Kaylee
               <br />
               Maczek
             </h1>
-            <p className="text-white text-sm sm:text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+            <p className="text-slate-300 text-sm sm:text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
               ipsam, similique dolore, optio labore praesentium ab libero
               aliquid animi reprehenderit minima amet adipisci neque sunt natus
@@ -84,14 +84,14 @@ export default function Creators() {
             src="https://fakeimg.pl/1200x1200"
           />
         </div>
-        <div className="hidden bg-slate-700 animate-[slideLeft_1.7s] lg:grid place-items-center">
+        <div className="hidden bg-slate-800 animate-[slideLeft_1.7s] lg:grid place-items-center">
           <div className="flex lg:flex-col justify-center items-center lg:items-start p-8 lg:p-16 gap-8 xl:gap-16">
             <h1 className="text-slate-200 text-2xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-medium tracking-wide">
               Kaylee
               <br />
               Maczek
             </h1>
-            <p className="text-white text-base sm:text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+            <p className="text-slate-300 text-base sm:text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
               ipsam, similique dolore, optio labore praesentium ab libero
               aliquid animi reprehenderit minima amet adipisci neque sunt natus
