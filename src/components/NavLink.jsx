01173b7b28be5ac34d470b349a19aa77464d89cb
alx-yng <alexyoung0119@gmail.com>
2024-03-01 16:17:43 -0600

    changed button font size

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -2,7 +2,7 @@ export default function NavLink({ path, text }) {
   return (
     <a
       href={path}
-      className="text-slate-500 hover:text-cyan-700 text-3xl hover:-translate-y-2 lg:hover:text-2xl transition duration-300"
+      className="text-slate-500 hover:text-cyan-700 text-xl hover:-translate-y-2 lg:hover:text-2xl transition duration-300"
     >
       {text}
     </a>
