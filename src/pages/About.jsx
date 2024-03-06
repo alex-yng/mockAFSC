484345361f576d674d0aa6528d48eb1b6cb54566
alx-yng <alexyoung0119@gmail.com>
2024-03-06 13:40:23 -0600

    fixed indentation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -65,115 +65,149 @@ export default function About() {
           </nav>
         )}
       </div>
-      
-      
-<section className=" container flex flex-wrap mt-10">
-  <div className="grid grid-cols-2 grid-rows-1 row items-start ml-10 place-items-center ">
-  <div className="grid grid-cols-1 grid-rows-none justify-start lg:w-full ">
-          <div>
-            <h1 className="text-3xl font-semibold text-slate-200 font-sans">
-              About Us
-            </h1>
-            <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 mr-16">
-              <p className="font-normal text-xl m-2">
-                The Ames Figure Skating Club is located at the Ames/ISU Ice
-                Arena, 1507 Gateway Hills Park Drive in Ames, Iowa. We provide
-                skaters of all ages and abilities many opportunities through our
-                Learn to Skate program along with the option of joining AFSC and
-                taking their skating to higher levels of skill and enjoyment.
-                Ames Figure Skating Club is dedicated to providing a safe, fun
-                and supportive environment for skaters of all ages and abilities
-                to achieve their skating goals.
-              </p>
+
+      <section className=" container flex flex-wrap mt-10">
+        <div className="grid grid-cols-2 grid-rows-1 row items-start ml-10 place-items-center ">
+          <div className="grid grid-cols-1 grid-rows-none justify-start lg:w-full ">
+            <div>
+              <h1 className="text-3xl font-semibold text-slate-200 font-sans">
+                About Us
+              </h1>
+              <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 mr-16">
+                <p className="font-normal text-xl m-2">
+                  The Ames Figure Skating Club is located at the Ames/ISU Ice
+                  Arena, 1507 Gateway Hills Park Drive in Ames, Iowa. We provide
+                  skaters of all ages and abilities many opportunities through
+                  our Learn to Skate program along with the option of joining
+                  AFSC and taking their skating to higher levels of skill and
+                  enjoyment. Ames Figure Skating Club is dedicated to providing
+                  a safe, fun and supportive environment for skaters of all ages
+                  and abilities to achieve their skating goals.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h1 className="text-3xl font-semibold text-slate-200 font-sans items-start">
+                Fall/Winter Events:
+              </h1>
+              <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 mr-16 ">
+                <p className="font-normal text-2xl m-2">
+                  <em className=" font-semibold text-2xl">
+                    ​Fall Kickoff Celebration & Breakfast
+                  </em>
+                  -<br /> This event is held in the fall as a chance to kick off
+                  our new skating season. <br />
+                  All club members are encouraged to attend.
+                  <br />
+                  During this event, we will review ice etiquette and ice
+                  monitor responsibilities as well as review club committees.
+                  <br />
+                  <br />
+                  <em className=" font-semibold text-2xl">
+                    Cyclone Championship
+                  </em>
+                  |<br /> Early November
+                  <br />
+                  <br />
+                  <em className=" font-semibold text-2xl">Holiday Gala</em>|
+                  <br /> Mid-December - This annual event is held to honor the
+                  holiday season.
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <h1 className="text-3xl font-semibold text-slate-200   font-sans">
+                Spring Events:
+              </h1>
+              <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 mr-16">
+                <p className="font-normal text-2xl m-2">
+                  <em className="font-semibold text-2xl">
+                    Great to Skate Event{" "}
+                  </em>
+                  | <br />
+                  Beginning of January - This annual event is to celebrate
+                  national skating month.
+                  <br />
+                  It is free admission and skate rental for everyone!
+                  <br />
+                  <br />
+                  <em className="font-semibold text-2xl">Spring Show</em> |{" "}
+                  <br />
+                  End of April - This annual event is held in the Spring each
+                  year and is a true ice production that
+                  <br />
+                  features all of our Club Skaters as well as Learn to Skate
+                  skaters.
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <h1 className="text-3xl font-semibold text-slate-200  font-sans">
+                Summer Events:
+              </h1>
+              <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 mr-16">
+                <p className="font-normal text-2xl m-2">
+                  <em className="font-semibold text-2xl">Summer Camps</em> |{" "}
+                  <br />
+                  June & July - Summer camps offered through AFSC is a chance
+                  for skaters to continue their skating
+                  <br />
+                  pursuits during the summer season. Skating is offered for all
+                  levels and is typically in the evening with both on and
+                  <br />
+                  off-ice opportunities.
+                  <br />
+                  <br />
+                  <em className="font-semibold text-2xl">Iowa Games</em> |{" "}
+                  <br />
+                  End of July - We host the annual Iowa Games competition.
+                </p>
+              </div>
             </div>
           </div>
-    <div>
-      <h1 className="text-3xl font-semibold text-slate-200 font-sans items-start">
-        Fall/Winter Events:
-      </h1>
-      <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 mr-16 ">
-        <p className="font-normal text-2xl m-2">
-          <em className=" font-semibold text-2xl">​Fall Kickoff Celebration & Breakfast</em>-<br />  This event is held in the fall as a chance to kick off our new skating season. <br />
-          All club members are encouraged to attend.<br />
-          During this event, we will review ice etiquette and ice monitor responsibilities as well as review club committees.<br />
-          <br />
-          <em className=" font-semibold text-2xl">Cyclone Championship</em>|<br />  Early November<br />
-          <br />
-          <em className=" font-semibold text-2xl">Holiday Gala</em>|<br />  Mid-December - This annual event is held to honor the holiday season.
-        </p>
-      </div>
-    </div>
-    
-  
-    <div>
-      <h1 className="text-3xl font-semibold text-slate-200   font-sans">
-        Spring Events:
-      </h1>
-      <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 mr-16">
-        <p className="font-normal text-2xl m-2">
-          <em className="font-semibold text-2xl">Great to Skate Event </em>| <br />Beginning of January - This annual event is to celebrate national skating month.<br />
-          It is free admission and skate rental for everyone!<br />
-          <br />
-          <em className="font-semibold text-2xl">Spring Show</em> | <br />End of April - This annual event is held in the Spring each year and is a true ice production that<br />
-          features all of our Club Skaters as well as Learn to Skate skaters.
-        </p>
-      </div>
-    </div>
-    
-    <div>
-      <h1 className="text-3xl font-semibold text-slate-200  font-sans">
-        Summer Events:
-      </h1>
-      <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 mr-16">
-        <p className="font-normal text-2xl m-2">
-        <em className="font-semibold text-2xl">Summer Camps</em> | <br />June & July - Summer camps offered through AFSC is a chance for skaters to continue their skating<br />
-          pursuits during the summer season. Skating is offered for all levels and is typically in the evening with both on and<br />
-          off-ice opportunities.<br />
-          <br />
-          <em className="font-semibold text-2xl">Iowa Games</em> | <br />End of July - We host the annual Iowa Games competition.
-        </p>
-      </div>
-    </div>
-    </div>
-  <div className="grid grid-cols-1 ml-auto items-end">
-  <div>
-            <h1 className="text-3xl font-semibold text-slate-200 ml-5 font-sans">
-              Become a Member!
-            </h1>
-            <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 ml-5 mr-10">
-              <p className="font-normal text-xl m-2">
-                Becoming a member of AFSC enables a skater to participate in U.S
-                Figure Skating events, including competitions and test sessions.
-                Skaters can participate in all AFSC sponsored events, such as
-                the ice show, Holiday Gala, and MORE! Members have access to
-                purchase club ice. This ice time is designated for private
-                lessons, small group lessons, and individual practice. Lastly,
-                you will be able to share in the sport of figure skating with
-                others and form lifelong friendships.
-              </p>
+          <div className="grid grid-cols-1 ml-auto items-end">
+            <div>
+              <h1 className="text-3xl font-semibold text-slate-200 ml-5 font-sans">
+                Become a Member!
+              </h1>
+              <div className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-slate-200  mx-auto m-4 ml-5 mr-10">
+                <p className="font-normal text-xl m-2">
+                  Becoming a member of AFSC enables a skater to participate in
+                  U.S Figure Skating events, including competitions and test
+                  sessions. Skaters can participate in all AFSC sponsored
+                  events, such as the ice show, Holiday Gala, and MORE! Members
+                  have access to purchase club ice. This ice time is designated
+                  for private lessons, small group lessons, and individual
+                  practice. Lastly, you will be able to share in the sport of
+                  figure skating with others and form lifelong friendships.
+                </p>
+              </div>
+            </div>
+            <div className=" lg:w-full flex flex-col max-w-[80%] space-x-28">
+              {aboutImgs.map((img) => {
+                return (
+                  <div
+                    key={Math.random() * 2178412}
+                    className=" mx-auto m-4 ml-5 mr-10 flex flex-col  place-items-center   "
+                  >
+                    <img src={img.url} alt={img.alt} />
+                    <p className="text-slate-200 font-extralight">
+                      {img.subtitle}
+                    </p>
+                  </div>
+                );
+              })}
             </div>
           </div>
-  <div className=" lg:w-full flex flex-col max-w-[80%] space-x-28">
-    {aboutImgs.map((img) => {
-      return (
-        <div
-          key={Math.random() * 2178412}
-          className=" mx-auto m-4 ml-5 mr-10 flex flex-col  place-items-center   "
-        >
-          <img src={img.url} alt={img.alt} />
-          <p className="text-slate-200 font-extralight">{img.subtitle}</p>
         </div>
-      );
-    })}
-  </div>
-</div>
-</div>  
-</section>
-<section>
-  <h1> 
-    <br />    
-  </h1>
-</section>
+      </section>
+      <section>
+        <h1>
+          <br />
+        </h1>
+      </section>
     </>
   );
 }
